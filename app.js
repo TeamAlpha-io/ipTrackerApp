b18496471a9d6d6f8509e1e44af63c7d601be615
eric <ericwang42@outlook.com>
2022-06-20 21:12:55 -0700

    styling & API update

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+"use strict";
+
+const inputField = document.querySelector(".inputField");
+const submitBtn = document.querySelector(".submitBtn");
+const ipAddress = document.querySelector(".ipAddress");
+const location = document.querySelector(".location");
+const timeZone = document.querySelector(".location");
+const isp = document.querySelector(".isp");
+
 //Leaflet Map
 const map = L.map("map").setView([51.505, -0.09], 13);
 
@@ -17,5 +26,20 @@ L.marker([51.5, -0.09])
 //     navigator.geolocation.getCurrentPosition(resolve, reject);
 //   });
 // };
-
 // getPosition().then((pos) => console.log(pos));
+
+// IPify API
+// https://geo.ipify.org/api/v2/country,city?apiKey=at_5azKAD4YK0mwQNBotQlfpXSZyww2C&ipAddress=8.8.8.8
+
+submitBtn.addEventListener("click", function () {
+  const getIpify = async () => {
+    const ipInput = inputField.value;
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_5azKAD4YK0mwQNBotQlfpXSZyww2C&ipAddress=${ipInput}`
+    );
+    const data = await res.json();
+    return data;
+  };
+});
+
+//TO DO: sort returned data(.ip, .location, .location.timezone, .isp) into appropriate DOM elements.
